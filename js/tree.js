40c2483c504b6073422b0289a1b5c5d09e2ad186
jagaleanov <jagaleanov@correo.udistrital.edu.co>
2021-04-10 18:01:48 -0500

    commit

--- js/tree.js
+++ js/tree.js
@@ -31,7 +31,6 @@ class Tree {
     constructor() {
         this.head = null;
         this.nodeCounter = 0;
-
         this.tableData = [[]];
         this.lineCounter = 0;
         this.shortNode = null;
@@ -59,7 +58,6 @@ class Tree {
     }
 
     addNode(parentId, value, weight) {
-
         if (parentId === "root") {
             if (this.head == null) {
                 this.nodeCounter++;
@@ -70,7 +68,6 @@ class Tree {
                 alert("El nodo ya esta ocupado.");
                 return false;
             }
-
         } else {
             let parentNode = this.findId(this.head, parentId);
             this.nodeCounter++;
@@ -85,7 +82,6 @@ class Tree {
                 while (temp.right != null) {
                     temp = temp.right;
                 }
-
                 newNode.parent = temp;
                 newNode.type = 'right';
                 temp.right = newNode;
@@ -97,58 +93,45 @@ class Tree {
     //ROUTES
     shortRoute(head) {
         if (head.down == null) {
-
             return [head, head.weight];
         } else {
-
             let node = head.down;
             let sum = 1000000000;
             let finalNode = null;
-
             do {
-
                 let resDown = this.shortRoute(node);
                 if (resDown[1] < sum) {
                     sum = resDown[1];
                     finalNode = resDown[0];
                 }
-
                 node = node.right;
             } while (node != null);
             return [finalNode, parseInt(sum) + parseInt(head.weight)];
-
         }
     }
 
     longRoute(head) {
         if (head.down == null) {
-
             return [head, head.weight];
         } else {
-
             let node = head.down;
             let sum = 0;
             let finalNode = null;
-
             do {
-
                 let resDown = this.longRoute(node);
                 if (resDown[1] > sum) {
                     sum = resDown[1];
                     finalNode = resDown[0];
                 }
-
                 node = node.right;
             } while (node != null);
             return [finalNode, parseInt(sum) + parseInt(head.weight)];
-
         }
     }
 
     //CREATE HTML BINARY
     toBinaryHTML(head) {
         var html = "";
-
         if (head === null) {
             return '<li><span class="px-2 py-1">*</span></li>';
         } else {
@@ -156,26 +139,20 @@ class Tree {
                 '<div class="node badge badge-pill badge-' + (head.id == this.shortNode[0].id ? 'success' : head.id == this.longNode[0].id ? 'danger' : 'primary') + '" ><small>' + head.weight + '</small><br>' +
                 head.value +
                 '</div>';
-
             if (head.down != null || head.right != null) {
-
                 html += '<ul>' +
                     this.toBinaryHTML(head.down) +
                     this.toBinaryHTML(head.right) +
-                    '</ul>' +
-                    '</li>';
+                    '</ul>';
             }
-
             html += '</li>';
         }
-
         return html;
     }
 
     //CREATE HTML BINARY
     toEnearyHTML(head) {
         var html = "";
-
         if (head === null) {
             return '<li><span class="px-2 py-1">*</span></li>';
         } else {
@@ -184,45 +161,32 @@ class Tree {
                 '<small>' + head.weight + '</small><br>' +
                 head.value +
                 '</div>';
-
             if (head.down != null) {
-
                 html += '<ul>';
                 let node = head.down;
-                do{
-
-
+                do {
                     html += this.toEnearyHTML(node);
 
                     node = node.right;
-                }while (node != null) ;
-                html += '</ul>' +
-                    '</li>';
+                } while (node != null);
+                html += '</ul>';
             }
-
             html += '</li>';
         }
-
         return html;
     }
-
 }
 
 var tree = new Tree();
 
 function addNode() {
-
-
     if ($('#valueTxt').val().length > 0 && ($('#weightTxt').val() || tree.head === null)) {
         tree.addNode($('#parentTxt').val(), $('#valueTxt').val(), $('#weightTxt').val());
-
         $('#formModal').modal('hide');
-
         printTrees();
     } else {
         alert('Ingrese valor y costo');
     }
-
 }
 
 function printTrees() {
@@ -234,18 +198,15 @@ function printTrees() {
         $('#addRootA').hide();//ocultar boton de insertar raiz
         $('#ulTreeA').html(tree.toEnearyHTML(tree.head));
         $('#ulTreeB').html(tree.toBinaryHTML(tree.head));//imprimir arbol
-
         let shortRoute = tree.shortRoute(tree.head);
         console.log(shortRoute);
         $('#shortValue').html(shortRoute[0].value);
         $('#shortWeight').html(shortRoute[1]);
-
         let longRoute = tree.longRoute(tree.head);
         console.log(shortRoute);
         $('#longValue').html(longRoute[0].value);
         $('#longWeight').html(longRoute[1]);
     }
-
 }
 
 tree.addNode('root', 'a', 0);
@@ -256,14 +217,11 @@ $('#formModal').on('show.bs.modal', function (event) {//listener botones
     var parent = button.data('parent'); // Extrae la info del atributo data-parent 
     var parentValue = button.data('parent-value'); // Extrae la info del atributo data-parent-value 
     var modal = $(this);
-
     modal.find('#valueTxt').val("");
     modal.find('#weightTxt').val("");
-
     if (parentValue == 'root') {
         modal.find('#weightTxt').val("0");
     }
-
     modal.find('.modal-title').text('Nuevo nodo en ' + parentValue + ' en el árbol.');
     modal.find('#parentTxt').val(parent);//llenar el campo parent oculto en el form 
 });
